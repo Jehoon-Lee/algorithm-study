fdca708c7cd81f10adec0728e83c44c689241a64
Jehoon-Lee <ljh5113@gmail.com>
2022-09-27 21:34:54 +0900

    220927 프로그래머스 1단계  부족한 금액 계산하기

--- 부족한 금액 계산하기.js
+++ 부족한 금액 계산하기.js
@@ -1,9 +1,7 @@
 function solution(price, money, count) {
-  let totalPrice = 0;
-  
+  let totalPrice = 0;  
   for(let i=1; i<count+1; i++) {
       totalPrice += price*i;
-  }
-  
+  } 
   return money-totalPrice > 0 ? 0 : Math.abs(money-totalPrice);
 }
