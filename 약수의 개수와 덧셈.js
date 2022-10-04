6d60593fc8b70f79394a039b99bfbc5411432e8b
Jehoon-Lee <ljh5113@gmail.com>
2022-10-04 21:52:38 +0900

    20221004 프로그래머스 1단계:약수의 개수와 덧셈(완료)

--- 약수의 개수와 덧셈.js
+++ 약수의 개수와 덧셈.js
@@ -1,6 +1,22 @@
+//내 답안
 function solution(left, right) {
-
+  let result = 0;
   for(let i=left; i<=right; i++) {
-      
+      let measure = []; //약수
+      for(let j=i; j>=1; j--) {
+          if (i % j === 0) measure.push(j);
+      }
+      measure.length % 2 === 0 ? result += i : result -= i;
   }
+  return result;
 }
+
+//다른 답안
+function solution(left, right) {
+  let result = 0;
+  for(let i=left; i<=right; i++) {
+      Number.isInteger(Math.sqrt(i)) ? result -= i : result += i;
+  }
+  return result;
+}
+
