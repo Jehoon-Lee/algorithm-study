5a5a892e3b4b4555334bc24f8b24e5120b3c2f96
Jehoon-Lee <ljh5113@gmail.com>
2025-04-01 10:16:15 +0900

    edit 간단한 논리 연산_181917.js

---
+++ programmers/간단한 논리 연산_181917.js
@@ -0,0 +1,27 @@
+function solution(x1, x2, x3, x4) {
+  const X = calc(x1, x2, 'OR');
+  const Y = calc(x3, x4, 'OR');
+  return calc(X, Y, 'AND');
+}
+
+function calc(a, b, type) {
+  let X = null;
+  // x V y (OR연산 계산)
+  if (type === 'OR') {
+    if (a === true) {
+      if (b === true || b === false) X = true;
+    } else if (a === false) {
+      if (b === true) X = true;
+      else if (b === false) X = false;
+    }
+  } //x ∧ y (AND연산 계산)
+  else if (type === 'AND') {
+    if (a === true) {
+      if (b === true) X = true;
+      else if (b === false) X = false;
+    } else if (a === false) {
+      if (b === true || b === false) X = false;
+    }
+  }
+  return X;
+}
